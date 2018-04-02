85a41dd098823ecf646cfe042a3a2421f644e5b0
vrajdalal4741 <vrajdalal.popeyes@gmail.com>
2018-04-02 13:32:12 -0400

    started js

---
+++ app/public/js/survey.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	
+
+	$('.submit-btn').on('click', function() {
+		event.preventDefault();
+		
+	})
+
+})
--- app/routing/apiRouting.js
+++ app/routing/apiRouting.js
@@ -5,6 +5,36 @@ const apiPaths = function (app) {
 	app.get('/people', function(request, response) {
 		response.json(people);
 	});
+
+	app.post('/api/people', function(request, response) {
+
+		let newPerson = request.body;
+		const newPersonAnswers = request.body.eval;
+		let finalScores = []; 
+	
+// for loops are only for arrays
+	for (var i = 0; i < people.length; i++) {
+		let differences = 0;
+		for (var j = 0; j < newPersonAnswers.length; j++) {
+			differences += Math.abs(parseInt(people[i].eval[j] - parseInt(newPersonAnswers[j])));
+		}
+		finalScores.push(differences);
+	};
+
+	let closestMatch = 0;
+	for (var i = 0; i < finalScores.length; i++) {
+		if (finalScores[i] <= finalScores[closestMatch]) {
+			closestMatch = i;
+		}
+	};
+
+	let matchedFriend = people[closestMatch];
+
+	people.push[newPerson];
+
+	response.json(matchedFriend);
+
+	});
 }
 
 module.exports = apiPaths;
