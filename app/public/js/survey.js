4b7884f421822c001a1a4631d0bca56a7b460039
vrajdalal4741 <vrajdalal.popeyes@gmail.com>
2018-04-17 10:33:04 -0400

    update

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -3,42 +3,58 @@ $(document).ready(function() {
 
 	$('.submit-btn').on('click', function() {
 		// jQuery.ajaxSettings.traditional = true;
+		//stopa page from reloading
 		event.preventDefault();
+		//
 
-		let newPersonName = $('#name').val().trim();
-
-		if (newPersonName.length === 0) {
+		if ($('#name').length === 0) {
 			alert("Please fill out your name!");
 
 		} else {
 
-			let answerChoices = $(".answers");
-			let answers = [];
-			Object.keys(answerChoices).forEach(function(input) {
-				if (answers.length < 10) {
-					answers.push(answerChoices[input].value)
-				}
-			});
-			console.log(answers);
+			let newPersonData = {
+			name: $('#name').val(),
+			eval: [
+				$('#ans1').val(),
+				$('#ans2').val(),
+				$('#ans3').val(),
+				$('#ans4').val(),
+				$('#ans5').val(),
+				$('#ans6').val(),
+				$('#ans7').val(),
+				$('#ans8').val(),
+				$('#ans9').val(),
+				$('#ans10').val()
+			]
 
-			let newPerson = {
-				'name': newPersonName,
-				'eval': answers
-			}
+		}
 
-			console.log(newPerson);
+		console.log(newPersonData);
+
+		// $.post('/people', newPersonData, function(data){
+		// 	console.log(data);
+		// })
+			// let answerChoices = $(".answers");
+			// let answers = [];
+			// Object.keys(answerChoices).forEach(function(input) {
+			// 	if (answers.length < 10) {
+			// 		answers.push(answerChoices[input].value)
+			// 	}
+			// });
+			// console.log(answers);
+
+			// let newPerson = {
+			// 	'name': newPersonName,
+			// 	'eval': answers
+			// }
+
+			// console.log(newPerson);
+
 
 			$.ajax({
-				'url': '/api/people',
+				'url': '/people',
 				'type': 'POST',
-				'data': {
-					'name': newPersonName,
-					'eval': answers
-				},
-				'dataType': 'json',
-				'success': function (result) {
-					console.log(result);
-				}
+				'data': newPersonData
 			})
 				}
 			})
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,43 +1,50 @@
 let path = require('path');
 let people = require('../data/friends.js');
 
-const apiPaths = function (app) {
-	app.get('/people', function(request, response) {
-		response.json(people);
-	});
+const apiPaths = function(app) {
 
-	app.post('/api/people', function(request, response) {
-		console.log(request.body);
-		let newPerson = request.body;
-		const newPersonAnswers = request.body.eval;
-		let finalScores = [];
-		console.log(newPersonAnswers);
-		console.log(request.body.eval);
+    app.get('/people', function(request, response) {
+        response.json(people);
+    });
 
-	
-// for loops are only for arrays
-	for (var i = 0; i < people.length; i++) {
-		let differences = 0;
-		for (var j = 0; j < newPersonAnswers.length; j++) {
-			differences += Math.abs(parseInt(people[i].eval[j] - parseInt(newPersonAnswers[j])));
-		}
-		finalScores.push(differences);
-	};
+    app.post('/people', function(request, response) {
+        console.log(request.body);
+        //creating new person and adding values
+        let newPersonData = request.body;
+        let newPersonAnswers = newPersonData.eval;
 
-	let closestMatch = 0;
-	for (var i = 0; i < finalScores.length; i++) {
-		if (finalScores[i] <= finalScores[closestMatch]) {
-			closestMatch = i;
-		}
-	};
 
-	let matchedFriend = people[closestMatch];
 
-	people.push[newPerson];
+        // let finalScores = [];
+        console.log(newPersonAnswers);
 
-	response.json(matchedFriend);
 
-	});
-}
+
+        // // for loops are only for arrays
+        // for (var i = 0; i < people.length; i++) {
+        //     let currentFriend = people[i];
+        //     let differences = 0;
+        //     console.log("friend" + currentFriend);
+            // for (var j = 0; j < newPersonAnswers.length; j++) {
+            // 	differences += Math.abs(parseInt(people[i].eval[j] - parseInt(newPersonAnswers[j])));
+            // }
+            // finalScores.push(differences);
+        // };
+
+        // 	let closestMatch = 0;
+        // 	for (var i = 0; i < finalScores.length; i++) {
+        // 		if (finalScores[i] <= finalScores[closestMatch]) {
+        // 			closestMatch = i;
+        // 		}
+        // 	};
+
+        // 	let matchedFriend = people[closestMatch];
+
+        	people.push(newPersonData);
+
+        // 	response.json(matchedFriend);
+
+    });
+};
 
 module.exports = apiPaths;
